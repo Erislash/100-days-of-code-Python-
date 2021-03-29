3751fb3480b4723f234fdf400d74df7569c7808b
erislash <eri.gim2008@gmail.com>
2021-03-29 01:00:53 -0300

    day 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,7 +1,6 @@
-n = input('Enter a number: ')
-acum = 0
-
-for l in n:
-    acum += int(l)
-
-print(acum)
+print('Welcome to the tip calculator')
+bill = float(input('What was the total bill? $'))
+percentage = int(input('What percentage tip would you like to give? '))
+split = int(input('How many people to split the bill? '))
+eachPersonPay = (bill + (percentage * bill / 100)) / split
+print(f'Each person must pay: ${round(eachPersonPay, 2)}')
